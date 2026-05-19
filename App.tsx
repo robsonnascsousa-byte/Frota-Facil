8bf14cec3a5203cae7b6ad18e468ed82fd0d2535
robsonnascsousa-byte <robson.nasc.sousa@gmail.com>
2026-05-19 09:39:34 -0300

    .

--- App.tsx
+++ App.tsx
@@ -374,7 +374,6 @@ const InnerApp: React.FC = () => {
     try {
       if (parcelas > 1) {
         const installmentValue = manutencao.valor / parcelas;
-        const parcelamento_id = `p-man-${Date.now()}`;
         for (let i = 0; i < parcelas; i++) {
           const date = new Date(manutencao.data + 'T12:00:00');
           date.setMonth(date.getMonth() + i);
@@ -384,7 +383,6 @@ const InnerApp: React.FC = () => {
             data: date.toISOString().split('T')[0],
             status: 'Em aberto',
             documentos_anexados: [],
-            parcelamento_id,
             tipo: `${manutencao.tipo} (${i + 1}/${parcelas})`
           });
         }
